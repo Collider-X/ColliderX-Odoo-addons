316939f1bd846dc0c8bcd624f454876341826baf
Marc Lijour <marc@lijour.net>
2017-12-04 13:11:02 -0500

    Website theme module skeleton

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # ColliderX-Odoo-addons
 This repository contains Odoo modules that builds the infrastructure of ColliderX.
 
+## cx_website_theme
+A module containing styling information. At this point it is only CSS, but it can contain more more.
+More information on [Odoo's documentation](http://www.odoo.com/documentation/10.0/howtos/themes.html).
+
 ## cx_website
 A module containing assets for the ColliderX website. It is possible to manage versioning from GitHub (a plus), but the code snippets don't seem to work anymore (in edit mode). That defeats the purpose of providing a simpler interface for non-coders. Not recommended.
 
---
+++ cx_website_theme/__manifest__.py
@@ -0,0 +1,54 @@
+# -*- encoding: utf-8 -*-
+#	The ColliderX website theme module (cx_website_theme) packs the ColliderX website theme on top of the base Odoo 11 community version.
+#	Copyright (C) 2017 Marc Lijour
+#   https://www.linkedin.com/in/marclijour
+#   https://github.com/Collider-X/ColliderX-Odoo-addons/cx_website
+#
+#	This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+{
+    'name': "ColliderX Website Theme",
+
+    'summary': """
+        This module packs the ColliderX website theme on top of the base Odoo 11 community version. 
+        It supports the ColliderX application (cx_app) with the website content.""",
+
+    'description': """
+        The ColliderX Website theme includes static images, CSS, Javascript, and other styling resources.
+    """,
+
+    'author': "Marc Lijour",
+    'website': "https://www.linkedin.com/in/marclijour/",
+    'license': "AGPL-3",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Website',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base',
+				'website',
+				'theme_default',	# we need the plain bootstrap theme
+				],
+
+    # always loaded
+    'data': [
+        'views/assets.xml',
+    ],
+
+    "auto_install": False,
+    "installable": True,
+}
+
