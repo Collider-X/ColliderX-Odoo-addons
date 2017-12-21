e7cf624400e1b59aed9a41c15b7f4dd5dd362d4a
Marc Lijour <marc@lijour.net>
2017-12-21 04:00:26 -0500

    cx_operations module

---
+++ cx_operations/__init__.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+#	The ColliderX main module (cx_operations) contains the ColliderX configuration data and apps for Odoo 11 (community).
+#	Copyright (C) 2017 Marc Lijour
+#   https://www.linkedin.com/in/marclijour
+#   https://github.com/Collider-X/ColliderX-Odoo-addons/cx_operations
+#
+#	This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
